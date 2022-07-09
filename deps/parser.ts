2051a5423260789af36b35962c6e69749bc8004f
Neko Of The Abyss <psi@innocent.com>
2022-07-09 16:58:28 +0530

    first commit

---
+++ deps/parser.ts
@@ -0,0 +1,6 @@
+export {
+  DOMParser,
+  Element,
+} from "https://deno.land/x/deno_dom@v0.1.31-alpha/deno-dom-wasm.ts";
+
+export { parse as parseExpression } from "https://deno.land/x/swc@0.2.0/mod.ts";
---
+++ deps/utils.ts
@@ -0,0 +1,1 @@
+export { BetterMap } from "https://deno.land/x/bettermap@v1.1.1/mod.ts";
